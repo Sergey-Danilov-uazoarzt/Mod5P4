b5f054a0872ba9f01b76398fd1dc813e61947a2a
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2023-03-31 21:51:04 +0300

    First commit

---
+++ src/Concat.java
@@ -0,0 +1,11 @@
+public class Concat {
+    public static void main(String[] args) {
+        String a = "12";
+        String b = "345";
+        String c = "678";
+
+        String number = a + "," + b + "," + c;
+
+        System.out.println(number);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+        List<String> people = Arrays.asList(
+                "Philip J. Fry",
+                "Turanga Leela",
+                "Bender Bending Rodriguez",
+                "Hubert Farnsworth",
+                "Hermes Conrad",
+                "John D. Zoidberg",
+                "Amy Wong"
+        );
+
+        String peopleString = String.join("; ", people);
+        System.out.println(peopleString);
+    }
+}
