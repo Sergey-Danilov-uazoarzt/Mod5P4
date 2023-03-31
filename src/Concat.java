9103248858da0142bb24eaa847187366c3778b36
Sergey-Danilov-uazoarzt <uazoarzt@mail.ru>
2023-03-31 22:04:49 +0300

    Scanner

--- src/Concat.java
+++ src/Concat.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 public class Concat {
     public static void main(String[] args) {
-        String a = "12";
-        String b = "345";
-        String c = "678";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите цифры три раза: ");
+        String a = sc.nextLine();
+        String b = sc.nextLine();
+        String c = sc.nextLine();
 
         String number = a + "," + b + "," + c;
 
